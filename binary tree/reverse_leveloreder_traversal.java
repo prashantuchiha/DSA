8457a16ed7a016eb55732c82c694e1eacbfd5c3c
prashantuchiha <prashant.20@st.niituniversity.in>
2023-04-27 23:01:32 +0530

    bt

---
+++ binary tree/reverse_leveloreder_traversal.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class reverse_leveloreder_traversal {
+    class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+    public class Tree
+    {
+        public ArrayList<Integer> reverseLevelOrder(Node node) 
+        {
+            // code here
+            ArrayList<Integer> list =new ArrayList<Integer>();
+            if(node==null){
+                return list;
+            }
+            
+            
+            return list;
+        }
+    }      
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
